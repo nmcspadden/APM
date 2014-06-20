c379b339528ee494130845e373ceaedbb4e3a39d
Nick McSpadden <nmcspadden@gmail.com>
2014-06-20 13:31:37 -0700

    Get recipe search dirs and repos

--- AutoPkgManager/APMAppDelegate.m
+++ AutoPkgManager/APMAppDelegate.m
@@ -13,6 +13,11 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    //get list of recipe search dirs, and recipe repos
+    NSArray* RecipeSearchDirs = (__bridge NSArray *)(CFPreferencesCopyAppValue(CFSTR("RECIPE_SEARCH_DIRS"), CFSTR("com.github.autopkg")));
+    NSDictionary* RecipeRepos = (__bridge NSDictionary *)(CFPreferencesCopyAppValue(CFSTR("RECIPE_REPOS"), CFSTR("com.github.autopkg")));
+    //get list of default processors by looking in /Library/AutoPkg/autopkglib for things ending in .py and not starting with __init__
+    
 }
 
 @end
