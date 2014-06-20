73052c1d61f270a69d7882cd8a0aa8c9a5064df0
Nick McSpadden <nmcspadden@gmail.com>
2014-06-20 09:31:39 -0700

    Initial Commit

---
+++ AutoPkgManager/APMAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  APMAppDelegate.m
+//  AutoPkgManager
+//
+//  Created by Nick McSpadden on 6/20/14.
+//  Copyright (c) 2014 Schools of the Sacred Heart. All rights reserved.
+//
+
+#import "APMAppDelegate.h"
+
+@implementation APMAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ AutoPkgManager/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AutoPkgManager
+//
+//  Created by Nick McSpadden on 6/20/14.
+//  Copyright (c) 2014 Schools of the Sacred Heart. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
