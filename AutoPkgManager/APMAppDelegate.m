5ed2a5bcd7291ae047fa19a51e0a5eb89bcd95f6
Nick McSpadden <nmcspadden@gmail.com>
2014-06-25 15:58:17 -0700

    Added APMProcessor class

--- AutoPkgManager/APMAppDelegate.m
+++ AutoPkgManager/APMAppDelegate.m
@@ -14,9 +14,14 @@
 {
     // Insert code here to initialize your application
     //get list of recipe search dirs, and recipe repos
-    NSArray* RecipeSearchDirs = (__bridge NSArray *)(CFPreferencesCopyAppValue(CFSTR("RECIPE_SEARCH_DIRS"), CFSTR("com.github.autopkg")));
-    NSDictionary* RecipeRepos = (__bridge NSDictionary *)(CFPreferencesCopyAppValue(CFSTR("RECIPE_REPOS"), CFSTR("com.github.autopkg")));
+    NSArray *RecipeSearchDirs = (__bridge NSArray *)(CFPreferencesCopyAppValue(CFSTR("RECIPE_SEARCH_DIRS"), CFSTR("com.github.autopkg")));
+    NSDictionary *RecipeRepos = (__bridge NSDictionary *)(CFPreferencesCopyAppValue(CFSTR("RECIPE_REPOS"), CFSTR("com.github.autopkg")));
+
     //get list of default processors by looking in /Library/AutoPkg/autopkglib for things ending in .py and not starting with __init__
+    NSArray *extensions = [NSArray arrayWithObjects:@"py", nil];
+    NSArray *dirContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:@"/Library/Autopkg/autopkglib/" error:nil];
+    NSArray *files = [dirContents filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"pathExtension IN %@", extensions]];
+    //note that "files" still contains __init__.py, which needs to be ignored
     
 }
 
---
+++ AutoPkgManager/APMProcessor.m
@@ -0,0 +1,19 @@
+//
+//  APMProcessor.m
+//  AutoPkgManager
+//
+//  Created by Nick McSpadden on 6/25/14.
+//  Copyright (c) 2014 Schools of the Sacred Heart. All rights reserved.
+//
+
+#import "APMProcessor.h"
+
+@implementation APMProcessor
+
+-(NSDictionary*) RetrieveDictionary
+{
+    
+    return NSDictionary* what;
+}
+
+@end
